2265044d102fcec8515c8be78647e2071a876428
blalfayed <fayedblal@yahoo.com>
2025-07-11 21:46:20 +0300

    create app styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100B20);
 
 const kTranitionDuration = Duration(milliseconds: 250);
+
+const KGtSectraFine = 'GT Sectra Fine';
