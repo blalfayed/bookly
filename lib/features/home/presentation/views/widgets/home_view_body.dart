544f3cdcdde6236a41f0cea477bb59ce7cccfcf9
blalfayed <fayedblal@yahoo.com>
2025-07-11 21:16:16 +0300

    best seller list view item part 1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -18,8 +19,36 @@ class HomeViewBody extends StatelessWidget {
             FeaturedBooksListView(),
             SizedBox(height: 50),
             Text("Best Seller", style: Styles.titleMedium),
+            BestSellerListViewItem(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.tafseer1),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
